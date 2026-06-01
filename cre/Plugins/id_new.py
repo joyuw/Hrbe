e2292359180772fa1e30e91802e2d3aecb61afda
joyuw <166529469+joyuw@users.noreply.github.com>
2026-06-01 17:55:35 +0300

    إصلاح أمر ID لعرض Custom Emoji بشكل صحيح

--- cre/Plugins/id_new.py
+++ cre/Plugins/id_new.py
@@ -26,10 +26,9 @@ def format_usernames(user_obj):
         return 'None'
     return ' ، '.join(usernames)
 
-# أمر الـ ID الجديد بدعم Custom Emoji
 @Client.on_message(filters.text & filters.group, group=13)
 def new_id_command(c, m):
-    """أمر ID جديد بدعم Custom Emoji والألوان"""
+    """أمر ID جديد بدعم Custom Emoji"""
     text = m.text
     k = r.get(f'{Dev_FLER}:botkey')
     
@@ -77,34 +76,39 @@ def new_id_command(c, m):
         except:
             create = 'غير متوفر'
         
-        # بناء رسالة الـ ID بـ Custom Emoji
-        # السطر الأول
-        text_line1 = f"يوزَر حسابَك ⌯ {_user}"
-        # السطر الثاني
-        text_line2 = f"عَدَد رسائلَك ⌯ {msgs}"
-        # السطر الثالث
-        text_line3 = f"رتبتَك ⌯ {_rank}"
-        # السطر الرابع
-        text_line4 = f"ايدي حسابَك ⌯ `{u.id}`"
-        # السطر الخامس
-        text_line5 = f"تَعديلاتَك ⌯ {edits}"
-        # السطر السادس
-        text_line6 = f"انشاء حسابَك ⌯ {create}"
+        # بناء رسالة الـ ID مع Custom Emoji IDs
+        # استخدام تاغ HTML لـ Custom Emoji
+        emoji_start_1 = '<emoji id="5794164805065514131">⌯</emoji>'
+        emoji_end_1 = '<emoji id="5891156376473836675">⌯</emoji>'
         
-        # دمج الأسطر مع Custom Emoji
-        message_text = f'''
-{text_line1}
-{text_line2}
-{text_line3}
-{text_line4}
-{text_line5}
-{text_line6}
+        emoji_start_2 = '<emoji id="5794085322400733645">⌯</emoji>'
+        emoji_end_2 = '<emoji id="6030537810509828330">⌯</emoji>'
+        
+        emoji_start_3 = '<emoji id="5794280000383358988">⌯</emoji>'
+        emoji_end_3 = '<emoji id="5084979757905347540">⌯</emoji>'
+        
+        emoji_start_4 = '<emoji id="5794241397217304511">⌯</emoji>'
+        
+        emoji_start_5 = '<emoji id="5793985348446984682">⌯</emoji>'
+        emoji_end_5 = '<emoji id="5769635757211784031">⌯</emoji>'
+        
+        emoji_start_6 = '<emoji id="5794324702402976226">⌯</emoji>'
+        
+        # بناء الرسالة مع الـ Custom Emoji
+        message_text = f'''<b>👤 معلومات حسابك</b>
+
+{emoji_start_1} يوزَر حسابَك ⌯ {_user} {emoji_end_1}
+{emoji_start_2} عَدَد رسائلَك ⌯ {msgs} {emoji_end_2}
+{emoji_start_3} رتبتَك ⌯ {_rank} {emoji_end_3}
+{emoji_start_4} ايدي حسابَك ⌯ <code>{u.id}</code>
+{emoji_start_5} تَعديلاتَك ⌯ {edits} {emoji_end_5}
+{emoji_start_6} انشاء حسابَك ⌯ {create}
 
 <i>Made with ❤️</i>
 '''
         
-        # إرسال الرسالة
-        return m.reply(message_text, disable_web_page_preview=True)
+        # إرسال الرسالة باستخدام HTML parsing
+        return m.reply(message_text, parse_mode=enums.ParseMode.HTML)
     
     except Exception as e:
         print(f"Error in new_id_command: {e}")
